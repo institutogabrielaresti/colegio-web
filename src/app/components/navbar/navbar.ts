3ccb3a59a5ab197ab7fe86754bcb737e202a5d8e
institutogabrielaresti <instituto.gabriel.aresti@gmail.com>
2026-03-05 12:07:30 -0600

    Actualizacion de Calendario

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,17 +1,33 @@
-import { Component, HostListener  } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit  } from '@angular/core';
+import { RouterLink, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { isPlatformBrowser } from '@angular/common';
+import { PLATFORM_ID, inject } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, CommonModule, RouterLink],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.css']
 })
 export class NavbarComponent {
 
+  platformId = inject(PLATFORM_ID);
+
+  constructor(private http: HttpClient) {}
+
   menuAbierto = false;
   scrolled = false;
+
+  mesActual = '';
+  anioActual = 0;
+  mesIndex = 0;
+  mostrarCalendario = false;
+  dias: number[] = [];
+  eventos: any[] = [];
+  eventoHover:any = null;
 
   toggleMenu() {
     this.menuAbierto = !this.menuAbierto;
@@ -27,43 +43,78 @@ export class NavbarComponent {
   }
 
   /*Calendario*/
-  mostrarCalendario = false;
-
-  dias: number[] = [];
-  mesActual = '';
-  eventos: any[] = [];
-
   toggleCalendario() {
     this.mostrarCalendario = !this.mostrarCalendario;
   }
 
   ngOnInit() {
     this.generarCalendario();
-    this.cargarEventos();
+    if (isPlatformBrowser(this.platformId)) {
+      this.cargarEventos();
+    }
   }
 
-  generarCalendario() {
+  generarCalendario(){
 
-    const hoy = new Date();
+  const hoy = new Date();
 
-    const año = hoy.getFullYear();
-    const mes = hoy.getMonth();
+  const año = this.anioActual || hoy.getFullYear();
+  const mes = this.mesIndex || hoy.getMonth();
 
-    const ultimoDia = new Date(año, mes + 1, 0).getDate();
+  const ultimoDia = new Date(año, mes + 1, 0).getDate();
 
-    this.mesActual = hoy.toLocaleString('es', { month: 'long' });
+  this.anioActual = año;
 
-    this.dias = Array.from({ length: ultimoDia }, (_, i) => i + 1);
+  this.mesActual = new Date(año, mes)
+    .toLocaleString('es', { month: 'long' });
 
+  this.dias = Array.from({ length: ultimoDia }, (_, i) => i + 1);
+}
+
+mesAnterior(){
+
+  this.mesIndex--;
+
+  if(this.mesIndex < 0){
+    this.mesIndex = 11;
+    this.anioActual--;
   }
 
-  async cargarEventos() {
+  this.generarCalendario();
 
-    const res = await fetch('assets/eventos.json');
-    const data = await res.json();
+}
 
-    this.eventos = data.eventos;
+mesSiguiente(){
 
+  this.mesIndex++;
+
+  if(this.mesIndex > 11){
+    this.mesIndex = 0;
+    this.anioActual++;
+  }
+
+  this.generarCalendario();
+
+}
+
+esHoy(dia:number){
+
+  const hoy = new Date();
+
+  return (
+    dia === hoy.getDate() &&
+    this.mesIndex === hoy.getMonth() &&
+    this.anioActual === hoy.getFullYear()
+  );
+
+}
+
+  cargarEventos() {
+
+  this.http.get<any>('assets/eventos.json')
+    .subscribe(data => {
+      this.eventos = data.eventos;
+    });
   }
 
   tieneEvento(dia:number){
@@ -77,4 +128,36 @@ export class NavbarComponent {
     return this.eventos.some(e => e.fecha === fecha);
 
   }
+
+mostrarEvento(dia:number){
+
+  const hoy = new Date();
+
+  const mes = this.mesIndex + 1;
+  const año = this.anioActual;
+
+  const fecha = `${año}-${String(mes).padStart(2,'0')}-${String(dia).padStart(2,'0')}`;
+
+  const evento = this.eventos.find(e => e.fecha === fecha);
+
+  if(evento){
+    this.eventoHover = {
+      dia,
+      titulo:evento.titulo
+    };
+  }
+
+}
+
+/*Salir del calendario con click*/
+@HostListener('document:click', ['$event'])
+cerrarCalendario(event:Event){
+
+  const elemento = event.target as HTMLElement;
+
+  if(!elemento.closest('.calendario-item')){
+    this.mostrarCalendario = false;
+  }
+
+}
 }
