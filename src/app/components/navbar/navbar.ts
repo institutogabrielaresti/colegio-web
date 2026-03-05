b31cda92067f1117470650579d4ea5e83f1127ec
institutogabrielaresti <instituto.gabriel.aresti@gmail.com>
2026-03-05 10:15:23 -0600

    Eventos

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -26,4 +26,55 @@ export class NavbarComponent {
     this.scrolled = window.scrollY > 50;
   }
 
+  /*Calendario*/
+  mostrarCalendario = false;
+
+  dias: number[] = [];
+  mesActual = '';
+  eventos: any[] = [];
+
+  toggleCalendario() {
+    this.mostrarCalendario = !this.mostrarCalendario;
+  }
+
+  ngOnInit() {
+    this.generarCalendario();
+    this.cargarEventos();
+  }
+
+  generarCalendario() {
+
+    const hoy = new Date();
+
+    const año = hoy.getFullYear();
+    const mes = hoy.getMonth();
+
+    const ultimoDia = new Date(año, mes + 1, 0).getDate();
+
+    this.mesActual = hoy.toLocaleString('es', { month: 'long' });
+
+    this.dias = Array.from({ length: ultimoDia }, (_, i) => i + 1);
+
+  }
+
+  async cargarEventos() {
+
+    const res = await fetch('assets/eventos.json');
+    const data = await res.json();
+
+    this.eventos = data.eventos;
+
+  }
+
+  tieneEvento(dia:number){
+
+    const hoy = new Date();
+    const mes = hoy.getMonth() + 1;
+    const año = hoy.getFullYear();
+
+    const fecha = `${año}-${String(mes).padStart(2,'0')}-${String(dia).padStart(2,'0')}`;
+
+    return this.eventos.some(e => e.fecha === fecha);
+
+  }
 }
